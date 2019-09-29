ea1b9b8732fae5c009b8a01cc31a46d99085fa99
Mac <mac@Macs-MacBook-Pro.local>
2019-09-29 16:28:30 +0800

    change--api-js

--- api.js
+++ api.js
@@ -1,7 +1,8 @@
 const people = [
     { name: 'Nader', age: 36 },
     { name: 'Amanda', age: 24 },
-    { name: 'Jason', age: 44 }
+    { name: 'Jason', age: 44 },
+    { name: 'Cheng', age: 30 }
 ]
 
 export default() => {
